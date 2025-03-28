c859c12b10f42f7d277ac5282f68b3a7773e7bbd
Henry Canapp <henrycanapp@ufl.edu>
2025-03-27 20:47:17 -0400

    Question 3 and 4 Completed

--- MaxMap.hpp
+++ MaxMap.hpp
@@ -7,9 +7,27 @@
 
 float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) {
     //implement your function here
+    float max = 0.0f;
+    if (data.find(search_term) != data.end()) {
+      for (size_t i = 0; i < data.at(search_term).size(); i++) {
+        if (data.at(search_term).at(i) > max) {
+          max = data.at(search_term).at(i);
+        }
+      }
+    } else {
+      max = -1.f;
+      }
+    return max;
 }
 
 std::map<std::string, float> returnMaxMap(const std::map<std::string, std::vector<float>>& in_map) {
     //implement your function here
-    
+    std::map<std::string, float> maxMap;
+    for (auto iter = in_map.begin(); iter != in_map.end(); iter++) {
+      float max = consultMax(iter->first, in_map);
+      if (max != -1) {
+        maxMap.emplace(iter->first, max);
+        }
+    }
+    return maxMap;
 }
--- TrafficData.hpp
+++ TrafficData.hpp
@@ -8,8 +8,24 @@
 
 std::map<std::string, int> loadTrafficData(const std::string& filename) {
   //implement your function here!
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "-1" << std::endl;
+  }
+  std::map<std::string, int> trafficData;
+  std::string line;
+  while (std::getline(file, line)) {
+    std::string airport_code = line.substr(line[0], line.find(','));
+    int numPassengers = stoi(line.substr(line.find(',') + 1, line.size()));
+    trafficData[airport_code] = numPassengers;
+  }
+  return trafficData;
 }
 
 void updateTrafficData(const std::string& filename, std::map<std::string, int>& dataMap) {
     //implement your function here!
+    auto trafficData = loadTrafficData(filename);
+    for (auto iter = trafficData.begin(); iter != trafficData.end(); iter++) {
+      dataMap[iter->first] = iter->second;
+    }
 }
